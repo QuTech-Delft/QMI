2074a5d34e6f1d4bba03ea427c2c7035872ce08c
Henri Ervasti <heevasti@tudelft.nl>
2023-10-09 11:14:59 +0200

    [QMI-043] Removed kwarg from USBError mock call in unit-tests.

--- tests/core/test_transport_usbtmc.py
+++ tests/core/test_transport_usbtmc.py
@@ -119,7 +119,7 @@ class TestQmiPyUsbTmcTransport(unittest.TestCase):
     @unittest.mock.patch("qmi.core.usbtmc.Instrument")
     def test_discard_read_does_not_except(self, mock):
         """See that discard_read passes QMI_TimeoutException if read buffer and incoming instrument buffer are empty."""
-        mock().read_raw.side_effect = [usb.core.USBError("Timeout!", errno=110)]
+        mock().read_raw.side_effect = [usb.core.USBError("Timeout!")]
         dev = QMI_PyUsbTmcTransport(0x1234, 0x5678, "90")
         dev.open()
         dev.discard_read()
