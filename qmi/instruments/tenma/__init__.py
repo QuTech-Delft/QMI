cea9202177c67b389ede4766a3fbf5252554f8e1
Henri Ervasti <heevasti@tudelft.nl>
2023-12-14 17:56:31 +0100

    [QMI-tnm] Created also implemented (sub)classes for the two requested models. Still need to create (sub-)subclasses for the other models with differing max voltage range.

---
+++ qmi/instruments/tenma/__init__.py
@@ -0,0 +1,10 @@
+"""
+Tenma (Newark Electronics) power supplies.
+
+The qmi.instruments.tenma package provides support for:
+- Series 72 power supplies
+  - 72-2535, 72-2540, 72-2545, 72-2550, 72-2925, 72-2930, 72-2935, 72-2940 & 72-10480
+  - 72-13350 & 72-13360
+"""
+
+from qmi.instruments.tenma.psu_72 import Tenma72_2550, Tenma72_13350
--- qmi/instruments/tenma/psu_72.py
+++ qmi/instruments/tenma/psu_72.py
@@ -1,12 +1,12 @@
 """Instrument driver for the Alice Pulse generator"""
 
 import logging
-import struct
+import re
 from time import sleep
-from typing import List, Optional, Union, Tuple
-from dataclasses import dataclass
+from typing import Any, Dict, List, Optional, Union, Tuple
 
 from qmi.core.context import QMI_Context
+from qmi.core.exceptions import QMI_InstrumentException
 from qmi.core.instrument import QMI_Instrument, QMI_InstrumentIdentification
 from qmi.core.rpc import rpc_method
 from qmi.core.transport import create_transport
@@ -27,49 +27,183 @@ class Tenma72_Base(QMI_Instrument):
     MAX_VOLTAGE = 0
     MAX_CURRENT = 0
 
-
-class Tenma72_2550(Tenma72_Base):
-    """Instrument driver for the Tenma72_2550"""
-    DEFAULT_BAUDRATE = 9600
-    MAX_VOLTAGE = 30  # V
-    MAX_CURRENT = 5  # A
-
     def __init__(self, context: QMI_Context, name: str, transport: str) -> None:
         super().__init__(context, name)
         self._transport = create_transport(
             transport, default_attributes={"baudrate": self.DEFAULT_BAUDRATE}
         )
-        self._enabled = False
-        self._status = {}
 
-    def _sendCommand(self, cmd: str) -> None:
+    def _send(self, cmd: str) -> None:
         """Writes a command to the Tenma, just an ascii string
 
         Parameters:
-            cmd:        The string to write.
+            cmd:    The command to write to the device.
         """
         self._transport.write(bytes(cmd, "ascii"))
-        sleep(.2)  # Sleep some bits to let the message arrive
+        sleep(.02)  # Sleep some bits to let the message arrive
 
-    def _getStatus(self):
-        """
-            Returns the power supply status as a dictionary of values
+    def _read(self, timeout: float = 0.2) -> str:
+        """Read the response from the device, try until response received or time-out.
 
-            * ch1Mode: "C.V | C.C"
-            * ch2Mode: "C.V | C.C"
-            * tracking:
-                * 00=Independent
-                * 01=Tracking series
-                * 10=Tracking parallel
-            * outEnabled: True | False
-            :return: Dictionary of status values
+        Parameters:
+            timeout: The time-out value for reading. Default is 200ms.
+
+        Returns:
+            response: Decoded response string.
         """
-        self._sendCommand("STATUS?")
+        response = self._transport.read_until_timeout(50, timeout)
+        return response.decode('ascii')
+
+    @rpc_method
+    def open(self) -> None:
+        self._check_is_closed()
+        _logger.info("Opening connection to %s", self._name)
+        self._transport.open()
+        super().open()
+
+    @rpc_method
+    def close(self) -> None:
+        self._check_is_open()
+        _logger.info("Closing connection to %s", self._name)
+        super().close()
+        self._transport.close()
+
+    @rpc_method
+    def get_idn(self) -> QMI_InstrumentIdentification:
+        """Get instrument identification info.
+
+        Returns:
+            QMI_InstrumentIdentification: NamedTuple with vendor, model, serial number and SW version info.
+        """
+        self._send("*IDN?")  # TODO: For 13350-13360 models it reads: MTTTH*DIN? Test!
+        idn = self._read()
+        # Find all relevant info from response. Should be of format TENMA 72‐2535 SN:1231345 V2.0
+        pattern = re.compile(r"(\A[^\W\d_]+) ([0-9-]+) SN:([\d]+) V([0-9.]+)")
+        vendor, model, serial, version = pattern.findall(idn)[0]
+        return QMI_InstrumentIdentification(
+            vendor=vendor,
+            model=model,
+            serial=serial,
+            version=version
+        )
+
+    @rpc_method
+    def get_status(self) -> Dict[str, Any]:
+        """This method needs to be implemented in the inheriting classes."""
+        raise NotImplementedError()
+
+    @rpc_method
+    def read_current(self, output_channel: Optional[int] = None) -> float:
+        """Read the current setting.
+
+        Parameters:
+            output_channel: The channel to send the inquiry to.
+
+        Returns:
+            Current setting of the channel in Amperes.
+        """
+        ch = output_channel or ""
+        command = f"ISET{ch}?"
+        self._send(command)
+        # Response has appended sixth byte from *IDN? to voltage reading due to a firmware bug
+        return float(self._read()[:5])
+
+    @rpc_method
+    def set_current(self, current: float, output_channel: Optional[int] = None) -> None:
+        """Sets the current setting.
+
+        Parameters:
+            current: Current for the channel in Amperes.
+            output_channel: The channel to send the inquiry to.
+
+        Raises:
+            ValueError: If the current setting is not between 0 and self.MAX_CURRENT.
+        """
+        if not 0 < current < self.MAX_CURRENT:
+            raise ValueError(f"Invalid current setting {current}, not between 0 and 5 A")
+
+        ch = output_channel or ""
+        command = f"ISET{ch}:{current:.3f}"
+        self._send(command)
+
+    @rpc_method
+    def read_voltage(self, output_channel: Optional[int] = None) -> float:
+        """Reads the voltage setting.
+
+        Parameters:
+            output_channel: The channel to send the inquiry to.
+
+        Returns:
+            Voltage setting of the channel in Volts.
+        """
+        ch = output_channel or ""
+        command = f"VSET{ch}?"
+        self._send(command)
+        return float(self._read())
+
+    @rpc_method
+    def set_voltage(self, voltage: float, output_channel: Optional[int] = None) -> None:
+        """Sets the voltage setting.
+
+        Parameters:
+            voltage: Voltage setting for the channel in Volts.
+            output_channel: The channel to send the inquiry to.
+
+        Raises:
+            ValueError: If the input voltage setting is not between 0 and self.MAX_VOLTAGE
+        """
+        if not 0 < voltage < self.MAX_VOLTAGE:
+            raise ValueError(f"Invalid current setting {voltage}, not between 0 and {self.MAX_VOLTAGE} V")
+
+        ch = output_channel or ""
+        command = f"VSET{ch}:{voltage:.3f}"
+        self._send(command)
+
+    @rpc_method
+    def enable_output(self, output: bool) -> None:
+        """
+        Parameters:
+            output_channel: The channel to send the inquiry to.
+
+                current: Current for the channel in Amps as a float
+        """
+        val = 1 if output else 0
+        command = f"OUT:{val}"
+        self._send(command)
+        status = self.get_status()
+        if status["OutputEnabled"] is not output:
+            raise QMI_InstrumentException("Setting output failed for an unknown reason")
+
+
+class Tenma72_2550(Tenma72_Base):
+    """Instrument driver for the Tenma 72-2550. The driver is tested with this model, but the respective
+    manual is also for models 72-2535, 72-2540, 72-2545, 72-2925, 72-2930, 72-2935, 72-2940 & 72-10480.
+    """
+    DEFAULT_BAUDRATE = 9600
+    MAX_VOLTAGE = 30  # V
+    MAX_CURRENT = 5  # A
+
+    def get_status(self) -> Dict[str, Any]:
+        """Get the power supply status as a dictionary of status values.
+
+        Dictionary composition is:
+            Ch1Mode: "C.V" | "C.C"
+            Ch2Mode: "C.V" | "C.C"
+            Tracking: xx, where xx is byte
+                * 00 = Independent
+                * 01 = Tracking series
+                * 10 = Tracking parallel
+            OutputEnabled: True | False
+        
+        Returns: 
+            Dictionary of status values.
+        """
+        self._send("STATUS?")
         statusByte = ord(self._transport.read(1, 1))  # Read response byte
         ch1mode = (statusByte & 0x01)
         ch2mode = (statusByte & 0x02)
         tracking = (statusByte & 0x0C) >> 2
-        out = (statusByte & 0x40)
+        output_enabled = (statusByte & 0x40)
 
         if tracking == 0:
             tracking = "Independent"
@@ -80,253 +214,52 @@ class Tenma72_2550(Tenma72_Base):
         else:
             tracking = "Unknown"
         return {
-            "ch1Mode": "C.V" if ch1mode else "C.C",
-            "ch2Mode": "C.V" if ch2mode else "C.C",
+            "Ch1Mode": "C.V" if ch1mode else "C.C",
+            "Ch2Mode": "C.V" if ch2mode else "C.C",
             "Tracking": tracking,
-            "outEnabled": bool(out),
+            "OutputEnabled": bool(output_enabled),
         }
 
-    def _read_str(self, timeout=0.2) -> str:
-        byte_stream = self._transport.read_until_timeout(50, timeout)
-        return byte_stream.decode('ascii')
 
-    def _initialize(self):
-        ## Read status
-        self._status = self._getStatus()
-        _logger.info(str(self._status))
-        self._enabled = self._status['outEnabled']
-
-    @rpc_method
-    def open(self) -> None:
-        self._check_is_closed()
-        _logger.info("Opening connection to %s", self._name)
-        self._transport.open()
-        # Initialize
-        super().open()
-        self._initialize()
-
-    @rpc_method
-    def close(self) -> None:
-        self._check_is_open()
-        _logger.info("Closing connection to %s", self._name)
-        super().close()
-        self._transport.close()
-
-    @rpc_method
-    def get_idn(self) -> QMI_InstrumentIdentification:
-        """Read instrument type and version and return QMI_InstrumentIdentification instance."""
-        return QMI_InstrumentIdentification(vendor="Qutech", model="Alice pulse generation", serial=None, version=1)
-
-    @rpc_method
-    def readCurrent(self) -> float:
-        """
-            Reads the current setting
-            :return: Current for the channel in Amps as a float
-        """
-        commandCheck = "ISET1?"
-        self._sendCommand(commandCheck)
-        # 72-2550 appends sixth byte from *IDN? to current reading due to firmware bug
-        return float(self._read_str()[:5])
-
-    @rpc_method
-    def setCurrent(self, current: float) -> float:
-        """
-            Sets the current setting, note the maximal accuracy is milliamps
-            Parameters:
-                current: Current for the channel in Amps as a float
-        """
-        if not 0 < current < self.MAX_CURRENT:
-            raise ValueError(f"Invalid current setting {current}, not between 0 and 5 A")
-
-        commandCheck = f"ISET1:{current:.3f}"
-        self._sendCommand(commandCheck)
-        return self.readCurrent()
-
-    @rpc_method
-    def readVoltage(self) -> float:
-        """
-            Reads the current setting
-            :return: Current for the channel in Amps as a float
-        """
-        commandCheck = "VSET1?"
-        self._sendCommand(commandCheck)
-        # 72-2550 appends sixth byte from *IDN? to current reading due to firmware bug
-        return float(self._read_str())
-
-    @rpc_method
-    def setVoltage(self, voltage: float) -> float:
-        """
-            Sets the current setting, note the maximal accuracy is milliamps
-            Parameters:
-                current: Current for the channel in Amps as a float
-        """
-        if not 0 < voltage < self.MAX_VOLTAGE:
-            raise ValueError(f"Invalid current setting {voltage}, not between 0 and 30 V")
-
-        commandCheck = f"VSET1:{voltage:.3f}"
-        self._sendCommand(commandCheck)
-        return self.readVoltage()
-
-    @rpc_method
-    def setOuput(self, output: bool) -> None:
-        """
-            Sets the current setting, note the maximal accuracy is milliamps
-            Parameters:
-                current: Current for the channel in Amps as a float
-        """
-        val = 1 if output else 0
-        commandCheck = f"OUT{val}"
-        self._sendCommand(commandCheck)
-        self._getStatus()
-        if self._enabled is not output:
-            raise QMI_InstrumentException("Setting output failed for an unknown reason")
-
-
-class Tenma72_13360(Tenma72_Base):
-    """Instrument driver for the Tenma72_13360"""
-    MAX_VOLTAGE = 30  # V
-    MAX_CURRENT = 5  # A
+class Tenma72_13350(Tenma72_Base):
+    """Instrument driver for the Tenma 72-13350. The driver is tested with this model, but manual is
+    also for the model 72-13360.
+    """
+    MAX_VOLTAGE = 30
+    MAX_CURRENT = 5
     DEFAULT_BAUDRATE = 115200
 
     def __init__(self, context: QMI_Context, name: str, transport: str) -> None:
-        super().__init__(context, name)
-        self._transport = create_transport(
-            transport, default_attributes={"baudrate": self.DEFAULT_BAUDRATE}
-        )
-        self._enabled = False
-        self._status = {}
+        super().__init__(context, name, transport)
 
-    def _sendCommand(self, cmd: str) -> None:
-        """Writes a command to the Tenma, just an ascii string
+    def get_status(self) -> Dict[str, Any]:
+        """Get the power supply status as a dictionary of status values.
 
-        Parameters:
-            cmd:        The string to write.
+        Dictionary composition is:
+            "ChannelMode ": "C.V" | "C.C",
+            "OutputEnabled": True | False,
+            "V/C priority ": "Current priority" | "Voltage priority",
+            "Beep ": True | False,
+            "Lock ": True | False,
+
+        Returns: 
+            Dictionary of status values.
         """
-        self._transport.write(bytes(cmd + '\n', "ascii"))
-        sleep(.2)  # Sleep some bits to let the message arrive
-
-    def _getStatus(self):
-        """
-            Returns the power supply status as a dictionary of values
-
-            "channelMode ": "C.V" or "C.C",
-            "output ": "ON" or "OFF",
-            "V/C priority ": "Current priority" or "Voltage priority",
-            "beep ": "ON" or "OFF",
-            "lock ": "ON" or "OFF",
-
-            :return: Dictionary of status values
-        """
-        self._sendCommand("STATUS?")
+        self._send("STATUS?")
         statusBytes = self._transport.read(2, 2)  # Read response byte
         # 72-13360 sends two bytes back, the second being '\n'
         status = statusBytes[0]
 
         ch1mode = (status & 0b00000001)
-        output = bool(status & 0b00000010)
+        output_enabled = bool(status & 0b00000010)
         current_priority = (status & 0b00000100)
         beep = bool(status & 0b00010000)
         lock = bool(status & 0b00100000)
 
         return {
-            "channelMode": "C.V" if ch1mode else "C.C",
-            "output": output,
+            "ChannelMode": "C.V" if ch1mode else "C.C",
+            "OutputEnabled": output_enabled,
             "V/C priority ": "Current priority" if current_priority else "Voltage priority",
-            "beep": beep,
-            "lock": lock,
+            "Beep": beep,
+            "Lock": lock,
         }
-
-    def _read_str(self, timeout=0.2) -> str:
-        byte_stream = self._transport.read_until_timeout(50, timeout)
-        return byte_stream.decode('ascii')
-
-    def _initialize(self):
-        ## Read status
-        self._status = self._getStatus()
-        _logger.info(str(self._status))
-        self._enabled = self._status['output']
-
-    @rpc_method
-    def open(self) -> None:
-        self._check_is_closed()
-        _logger.info("Opening connection to %s", self._name)
-        self._transport.open()
-        # Initialize
-        super().open()
-        self._initialize()
-
-    @rpc_method
-    def close(self) -> None:
-        self._check_is_open()
-        _logger.info("Closing connection to %s", self._name)
-        super().close()
-        self._transport.close()
-
-    @rpc_method
-    def get_idn(self) -> QMI_InstrumentIdentification:
-        """Read instrument type and version and return QMI_InstrumentIdentification instance."""
-        return QMI_InstrumentIdentification(vendor="Tenma", model="72-13360", serial=None, version=1)
-
-    @rpc_method
-    def readCurrent(self) -> float:
-        """
-            Reads the current setting
-            :return: Current for the channel in Amps as a float
-        """
-        commandCheck = "ISET?"
-        self._sendCommand(commandCheck)
-        return float(self._read_str()[:5])
-
-    @rpc_method
-    def setCurrent(self, current: float) -> float:
-        """
-            Sets the current setting, note the maximal accuracy is milliamps
-            Parameters:
-                current: Current for the channel in Amps as a float
-        """
-        if not 0 < current < self.MAX_CURRENT:
-            raise ValueError(f"Invalid current setting {current}, not between 0 and 5 A")
-
-        commandCheck = f"ISET:{current:.3f}"
-        self._sendCommand(commandCheck)
-        return self.readCurrent()
-
-    @rpc_method
-    def readVoltage(self) -> float:
-        """
-            Reads the current setting
-            :return: Current for the channel in Amps as a float
-        """
-        commandCheck = "VSET?"
-        self._sendCommand(commandCheck)
-        return float(self._read_str())
-
-    @rpc_method
-    def setVoltage(self, voltage: float) -> float:
-        """
-            Sets the current setting, note the maximal accuracy is milliamps
-            Parameters:
-                current: Current for the channel in Amps as a float
-        """
-        if not 0 < voltage < self.MAX_VOLTAGE:
-            raise ValueError(f"Invalid current setting {voltage}, not between 0 and 30 V")
-
-        commandCheck = f"VSET:{voltage:.3f}"
-        self._sendCommand(commandCheck)
-        return self.readVoltage()
-
-    @rpc_method
-    def setOuput(self, output: bool) -> None:
-        """
-            Sets the current setting, note the maximal accuracy is milliamps
-            Parameters:
-                current: Current for the channel in Amps as a float
-        """
-        val = 1 if output else 0
-        commandCheck = f"OUT:{val}"
-        self._sendCommand(commandCheck)
-        self._status = self._getStatus()
-        _logger.info(str(self._status))
-        self._enabled = self._status['output']
-        if self._enabled is not output:
-            raise QMI_InstrumentException("Setting output failed for an unknown reason")
