9570395e19739746f1e9886b7c235a6921bc81c9
Henri Ervasti <heevasti@tudelft.nl>
2024-01-19 09:56:37 +0100

    [QMI-065] Changed the `globals()` to `import *` also in the `__init__.py`.

--- qmi/instruments/tenma/__init__.py
+++ qmi/instruments/tenma/__init__.py
@@ -6,10 +6,4 @@ The qmi.instruments.tenma package provides support for:
   - 72-2535, 72-2540, 72-2545, 72-2550, 72-2925, 72-2930, 72-2935, 72-2940 & 72-10480
   - 72-13350 & 72-13360
 """
-import importlib
-
-tenma_module = importlib.import_module("qmi.instruments.tenma.psu_72")
-psu_classes = {name: cls for name, cls in dict(vars(tenma_module)).items() if name.startswith("Tenma")}
-for k, v in psu_classes.items():
-    if "Base" not in k:
-        globals()[k] = v
+from qmi.instruments.tenma.psu_72 import *
