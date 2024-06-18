787987f149233597fb7cc39c0e4c887e62a914ce
Ravi Budhrani <rbudhrani@connect.ust.hk>
2024-06-18 16:39:03 +0200

    wip porting old zicore functions

--- qmi/instruments/zurich_instruments/hdawg.py
+++ qmi/instruments/zurich_instruments/hdawg.py
@@ -80,6 +80,72 @@ class ZurichInstruments_HDAWG(QMI_Instrument):
         # Connect to Zurich Instruments Data Server
         self._session = zhinst.toolkit.Session(self._server_host, self._server_port)
 
+    def _get_awg_node(self, awg_channel: int) -> "zhinst.toolkit.driver.nodes.awg.AWG":
+        """
+        Get the AWG node using the core index.
+
+        Parameters:
+            awg_channel:    The AWG channel to get the node for.
+
+        Returns:
+            AWG node
+        """
+        return self._device.awgs[self.CHANNEL_TO_CORE_MAPPING[awg_channel]]
+
+    def _get_int(self, awg_channel: int, node_path: str) -> int:
+        """
+        Get an integer value from the nodetree.
+
+        Parameters:
+            awg_channel:    The AWG channel to get the node for.
+            node_path:      The path to the node to be queried.
+
+        Returns:
+            integer value from node tree
+        """
+        awg_node = self._get_awg_node(awg_channel)
+        return awg_node.root.connection.getInt(node_path)
+
+    def _get_double(self, awg_channel: int, node_path: str) -> float:
+        """
+        Get a double value from the nodetree.
+
+        Parameters:
+            awg_channel:    The AWG channel to get the node for.
+            node_path:      The path to the node to be queried.
+
+        Returns:
+            double value from node tree.
+        """
+        awg_node = self._get_awg_node(awg_channel)
+        return awg_node.root.connection.getDouble(node_path)
+
+    def _get_string(self, awg_channel: int, node_path: str) -> str:
+        """
+        Get a string value from the nodetree.
+
+        Parameters:
+            awg_channel:    The AWG channel to get the node for.
+            node_path:      The path to the node to be queried.
+
+        Returns:
+            string value from node tree.
+        """
+        awg_node = self._get_awg_node(awg_channel)
+        return awg_node.root.connection.getString(node_path)
+
+    def _set_value(self, awg_channel: int, node_path: str, value: Union[str, int, float]) -> None:
+        """
+        Set an integer value from the nodetree.
+
+        Parameters:
+            awg_channel:    The AWG channel to get the node for.
+            node_path:      The path to the node to be queried.
+            value:          Value to set for the node.
+        """
+        awg_node = self._get_awg_node(awg_channel)
+        awg_node.root.connection.set(node_path)
+
     @staticmethod
     def get_sequence_snippet(waveforms: "zhinst.toolkit.Waveforms") -> str:
         """
@@ -114,18 +180,6 @@ class ZurichInstruments_HDAWG(QMI_Instrument):
         self._session.disconnect_device(self._device_name)
 
         super().close()
-
-    def _get_awg_node(self, awg_channel: int) -> "zhinst.toolkit.driver.nodes.awg.AWG":
-        """
-        Get the AWG node using the core index.
-
-        Parameters:
-            awg_channel:   The AWG channel to get the node for.
-
-        Returns:
-            AWG node
-        """
-        return self._device.awgs[self.CHANNEL_TO_CORE_MAPPING[awg_channel]]
 
     @rpc_method
     def compile_and_upload(self, awg_channel: int, sequencer_program: Union[str, "zhinst.toolkit.Sequence"]) -> None:
@@ -297,3 +351,124 @@ class ZurichInstruments_HDAWG(QMI_Instrument):
         awg_node = self._get_awg_node(awg_channel)
 
         return awg_node.commandtable.load_from_device()
+
+    @rpc_method
+    def get_node_int(self, awg_channel: int, node_path: str) -> int:
+        """
+        Get an integer value for the node.
+
+        Parameters:
+            awg_channel:    The AWG channel to which the node belongs.
+            node_path:      The node to query.
+
+        Returns:
+            integer value for the given node.
+        """
+        self._check_is_open()
+        _logger.info("[%s] Getting integer value for node [%s]", self._name, node_path)
+
+        return self._get_int(awg_channel, node_path)
+
+    @rpc_method
+    def get_node_double(self, awg_channel: int, node_path: str) -> float:
+        """
+        Get a double value for the node.
+
+        Parameters:
+            awg_channel:    The AWG channel to which the node belongs.
+            node_path:      The node to query.
+
+        Returns:
+            double value for the given node.
+        """
+        self._check_is_open()
+        _logger.info("[%s] Getting double value for node [%s]", self._name, node_path)
+
+        return self._get_int(awg_channel, node_path)
+
+    @rpc_method
+    def get_node_string(self, awg_channel: int, node_path: str) -> float:
+        """
+        Get a string value for the node.
+
+        Parameters:
+            awg_channel:    The AWG channel to which the node belongs.
+            node_path:      The node to query.
+
+        Returns:
+            string value for the given node.
+        """
+        self._check_is_open()
+        _logger.info("[%s] Getting string value for node [%s]", self._name, node_path)
+
+        return self._get_string(awg_channel, node_path)
+
+    @rpc_method
+    def sync(self) -> None:
+        """
+        Synchronise the state of the AWG.
+        This call ensures that all previous settings have taken effect on
+        the instrument, and stale data is flushed from local buffers.
+        The sync is performed for all devices connected to the DAQ server
+
+        Parameters:
+            awg_channel:    The AWG channel to which the node belongs.
+        """
+        self._check_is_open()
+        _logger.info("[%s] Synchronising state of AWG", self._name)
+        self._session.sync()
+
+    @rpc_method
+    def set_reference_clock_source(self, value: int) -> None:
+        """
+        Set the clock to be used as the the frequency and timebase reference.
+
+        Parameters:
+            value:  Clock source to select.
+                    0 = internal reference clock;
+                    1 = external 10MHz or 100MHz reference clock;
+                    2 = a ZSync clock.
+        """
+        if value not in (0, 1):
+            raise ValueError("Unsupported reference clock source")
+        _logger.info("[%s] Setting reference clock source to [%d]", self._name, value)
+        self._device.system.clocks.referenceclock.source(value)
+
+    @rpc_method
+    def get_reference_clock_status(self) -> int:
+        """Return the status of the reference clock.
+        Returns:
+            0 if the reference clock is locked;
+            1 if there was an error locking to the reference clock;
+            2 if the device is busy locking to the reference clock.
+        """
+        self._check_is_open()
+        _logger.info("[%s] Getting status of reference clock", self._name)
+        return self._device.system.clocks.referenceclock.status
+
+    @rpc_method
+    def set_sample_clock_frequency(self, frequency: float) -> None:
+        """
+        Set the base sample clock frequency.
+        Changing the sample clock temporarily interrupts the AWG sequencers.
+
+        Parameters:
+            value: New base sample clock frequency in Hz (range 100.0e6 to 2.4e9).
+        """
+        self._check_is_open()
+        _logger.info("[%s] Setting sample clock frequency to [%d]", self._name, frequency)
+        self._device.system.clocks.sampleclock.freq(frequency)
+
+    @rpc_method
+    def get_sample_clock_status(self) -> int:
+        """
+        Get the status of the sample clock.
+
+        Returns:
+            0 if the sample clock is valid and locked;
+            1 if there was an error adjusting the sample clock;
+            2 if the device is busy adjusting the sample clock.
+        """
+        self._check_is_open()
+        _logger.info("[%s] Getting status of sample clock", self._name)
+        return self._device.system.clocks.sampleclock.status
