7411eb8c4668cda750281893a753a62dd4708bbd
Henri Ervasti <heevasti@tudelft.nl>
2024-07-16 16:16:47 +0200

    [#QMI-xxx] Removing problematic code.

--- qmi/instruments/high_finesse/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-High Finesse, Wavelength meter.
-
-The qmi.instruments.high_finesse package provides support for:
-  - HighFinesse WLM Controller model WS-6 (tested).
-  - HighFinesse WLM Controller model WS-5, WS-7, WS-8 (untested).
-"""
-from qmi.instruments.high_finesse.support import wlmData
-from qmi.instruments.high_finesse.wlm import HighFinesse_WS
