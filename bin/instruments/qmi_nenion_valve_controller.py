dad8a5c3535459cb23c60b73f7b891d95eec6721
heevasti <h.k.ervasti@tudeflt.nl>
2024-11-12 15:27:38 +0100

    [QMI-117] Added CLI for testing and some edits on the driver.

---
+++ bin/instruments/qmi_nenion_valve_controller.py
@@ -0,0 +1,98 @@
+#! /usr/bin/env python
+
+import argparse
+import sys
+
+import qmi
+from qmi.core.exceptions import QMI_ApplicationException, QMI_UsageException
+from qmi.utils.context_managers import start_stop
+from qmi.instruments.nenion import Nenion_ValveController
+
+
+def main() -> int:
+
+    parser = argparse.ArgumentParser()
+    parser.description = "Command line client for the Tenma 72 series Power Supply Units."
+
+    address = parser.add_mutually_exclusive_group(required=True)
+    address.add_argument("--tcp", type=str, help="IP address of device.")
+    address.add_argument("--serial", type=str, help="(USB-to-)Serial address of device (COMx, /dev/ttySx).")
+
+    conn_param = parser.add_mutually_exclusive_group(required=False)
+    conn_param.add_argument("--port", type=str, help="IP address port number.", default=1512)
+    conn_param.add_argument("--baud", type=str, help="Baud rate of the device connection.", default=115200)
+
+    # parser.add_argument("--status", action="store_true", help="Get instrument status")
+    # setters/getters
+    motor_current = parser.add_mutually_exclusive_group(required=False)
+    motor_current.add_argument("--enable", action="store_true", help="Enable motor current")
+    motor_current.add_argument("--disable", action="store_true", help="Disable motor current")
+
+    parser.add_argument("--halt", action="store_true", help="Halt movement immediately")
+    parser.add_argument("--fully_close", action="store_true", help="'Null' valve by closing it fully.")
+    parser.add_argument("-t", "--target", type=int, help="Set valve open percentage target", nargs=1)
+    steps = parser.add_mutually_exclusive_group(required=False)
+    steps.add_argument("-p", "--step_open", type=int, help="Step towards open", nargs='?', const=1)
+    steps.add_argument("-m", "--step_close", type=int, help="Step towards close", nargs='?', const=1)
+    args = parser.parse_args()
+    print(args.tcp, args.port)
+    print(args.serial, args.baud)
+    print(args.enable, args.disable)
+    print(args.halt)
+    print(args.fully_close)
+    print(args.target)
+    print(args.step_open)
+    print(args.step_close)
+
+    with start_stop(qmi, "nenion_valve_controller_client", console_loglevel="WARNING"):
+        if args.tcp:
+            transport = f"tcp:{args.tcp}:{args.port}"
+
+        elif args.serial:
+            transport = f"serial:{args.serial}"
+            if args.baud != 115200:
+                transport += f":baudrate={args.baud}:parity=E"
+
+        print(transport, transport.startswith("tcp"), transport.startswith("serial"))
+        with Nenion_ValveController(qmi.context(), "Nenion_Valve_Controller", transport) as instr:
+            if args.halt:
+                print("Halting motor.")
+                instr.halt_motor()
+                if args.disable:
+                    instr.disable_motor_current()
+
+            if args.halt and any([args.enable, args.fully_close, args.target, args.step_open, args.step_close]):
+                raise QMI_UsageException("Cannot halt and enable current or drive at the same time!")
+
+            if args.enable:
+                print("Enabling motor current")
+                instr.enable_motor_current()
+
+            elif args.disable:
+                print("Disabling motor current")
+                instr.disable_motor_current()
+
+            if args.disable and any([args.fully_close, args.target, args.step_close, args.step_open]):
+                raise QMI_UsageException("Cannot drive after disabling current.")
+
+            if args.fully_close and any([args.target, args.step_close, args.step_open]):
+                raise QMI_UsageException("Full close and drive to target or stepping not allowed at the same time!")
+
+            if args.target:
+                print(f"Moving to target percentage {args.target[0]}")
+                instr.open_to_target(args.target[0])
+
+            if args.step_open:
+                print(f"stepping +{args.step_open} steps.")
+                instr.step_open(args.step_open)
+
+            if args.step_close:
+                print(f"stepping -{args.step_close} steps.")
+                instr.step_close(args.step_close)
+
+    return 0
+
+
+
+if __name__ == "__main__":
+    sys.exit(main())
--- qmi/instruments/nenion/valve_controller.py
+++ qmi/instruments/nenion/valve_controller.py
@@ -1,8 +1,10 @@
 """QMI_Instrument driver for Nenion stepper motor driver leak valve controllers."""
 
 import logging
+import time
 
 from qmi.core.context import QMI_Context
+from qmi.core.exceptions import QMI_TransportDescriptorException, QMI_InstrumentException
 from qmi.core.instrument import QMI_Instrument, QMI_InstrumentIdentification
 from qmi.core.rpc import rpc_method
 from qmi.core.transport import create_transport
@@ -14,27 +16,34 @@ _logger = logging.getLogger(__name__)
 class Nenion_ValveController(QMI_Instrument):
     """QMI_Instrument driver class for Nenion valve controllers."""
 
-    # default response timeout in seconds
-    DEFAULT_RESPONSE_TIMEOUT = 5.0
-    VALVE_RESOLUTION = 400  # The valve range of 0-100 % is divided into 40000 steps
+    DEFAULT_RESPONSE_TIMEOUT = 5.0  # default response timeout in seconds
+    VALVE_RANGE = [1, 40000]  # Serial/TCP interface valve range.
+    VALVE_RESOLUTION = VALVE_RANGE[1] // 100  # The valve range is divided into 0-100%, resolution is 1%.
+    STEP_RESOLUTION = VALVE_RESOLUTION // 10 # The step resolution is 0.1%.
+    MOVE_SPEED = 400.0  # Approximate valve move speed, in steps/second.
 
     def __init__(self, context: QMI_Context, name: str, transport: str):
         """Initialise driver.
 
         Parameters:
-            transport:  QMI transport descriptor to connect to the instrument.
+            transport:  QMI transport descriptor to connect to the instrument. Both serial and TCP are accepted.
         """
         super().__init__(context, name)
         self._timeout = self.DEFAULT_RESPONSE_TIMEOUT
         self._transport_str = transport
+        if not transport.startswith("tcp") and not transport.startswith("serial"):
+            raise QMI_TransportDescriptorException("Only serial and TCP transports accepted.")
+
         self._transport = create_transport(transport)
         # Communication defaults
         self.message_terminator = b"\r"
+        # Position tracking
+        self._current_position = None  # TODO: Always 'null' at start to be able to track from 0%?
 
     def _set(self, command: str):
         """Helper function for inspecting commands and their return values."""
         self._transport.write(command.encode('ascii') + self.message_terminator)
-        response = self._transport.read_until(self.message_terminator, self._timeout)
+        response = self._transport.read_until(self.message_terminator, self._timeout).decode()
         print(f"Controller command {command} resulted in response {response}.")  # For testing with HW
         _logger.debug("Controller command %s resulted in response %s." % command, response)
         # TODO: Depending on responses, create conditional behaviour
@@ -60,25 +69,36 @@ class Nenion_ValveController(QMI_Instrument):
         self._set("D")
 
     @rpc_method
-    def valve_open_percentage(self, target: int) -> None:
-        """Set the target percentage value for valve opening.
+    def open_to_target(self, target: int) -> None:
+        """Set the target percentage value for valve opening and drive.
 
         Parameters:
             target: The target percentage in range [0-100%].
+
+        Raises:
+            ValueError: By invalid percentage, must be between 0-100%.
         """
         if target not in range(0, 101):
             raise ValueError(f"Target percentage {target} is not a valid value!")
 
         target_step = target * self.VALVE_RESOLUTION
         if target_step == 0:
-            target_step = 1  # Needs to be at least 1
+            self._set(f"G1")  # According to manual 1 is the minimum, not 0.
 
-        self._set(f"G{target_step}")
+        else:
+            self._set(f"G{target_step}")
+
+        # TODO: Need some sleep afterwards?
+        # if self._current_position is not None:
+        #     time.sleep(abs(self._current_position - target_step) / self.MOVE_SPEED)
+
+        self._current_position = target_step
 
     @rpc_method
     def fully_close(self) -> None:
         """Special command that calls "Null" to close the valve at 1/3rd of max speed."""
         self._set("N")
+        self._current_position = 0  # Actually it is 1, but to avoid shift by 1 for consecutive steps, set to 0.
 
     @rpc_method
     def halt_motor(self) -> None:
@@ -92,12 +112,22 @@ class Nenion_ValveController(QMI_Instrument):
 
         Parameters:
             steps: How many steps should the controller do. Default is 1.
+
+        Raises:
+            ValueError: By invalid number of steps. Must be in range 1 to 9.
         """
         if steps not in range(1, 10):
             raise ValueError(f"Invalid steps, {steps}, given for controller.")
 
         for _ in range(steps):
+            if self._current_position is not None:
+                if self._current_position - self.STEP_RESOLUTION < self.VALVE_RANGE[0]:
+                    raise QMI_InstrumentException("Target position will be beyond limits. Excepting!")
+
+                self._current_position -= self.STEP_RESOLUTION
+
             self._set("M")  # TODO: Need some sleep between steps?
+            # time.sleep(self.STEP_RESOLUTION / self.MOVE_SPEED)
 
     @rpc_method
     def step_open(self, steps: int = 1):
@@ -111,4 +141,12 @@ class Nenion_ValveController(QMI_Instrument):
             raise ValueError(f"Invalid steps, {steps}, given for controller.")
 
         for _ in range(steps):
+            if self._current_position is not None:
+                if self._current_position + self.STEP_RESOLUTION > self.VALVE_RANGE[1]:
+                    raise QMI_InstrumentException("Target position will be beyond limits. Excepting!")
+
+                self._current_position += self.STEP_RESOLUTION
+
             self._set("P")  # TODO: Need some sleep between steps?
+            # time.sleep(self.STEP_RESOLUTION / self.MOVE_SPEED)
+
