5fcb5dadcabfaca3a4e15d2b182a204c5ed6eb9f
heevasti <h.k.ervasti@tudeflt.nl>
2025-04-01 12:29:07 +0200

    [QMI-140] Added testing for the 'filter' function in _RateLimitFilter class.

--- tests/core/test_logging_init.py
+++ tests/core/test_logging_init.py
@@ -2,6 +2,9 @@ import unittest
 import unittest.mock
 
 import logging
+from collections import namedtuple
+from time import monotonic, sleep
+
 from qmi.core.logging_init import start_logging, _RateLimitFilter
 
 
@@ -57,7 +60,6 @@ class TestStartLoggingOptions(unittest.TestCase):
         logging_patch.captureWarnings.assert_called_with(True)
 
     @unittest.mock.patch("qmi.core.logging_init.logging")
-    # @unittest.mock.patch("qmi.core.logging_init.logging.Filter")
     def test_logging_rate_limit(self, logging_patch):
         """Test that a logger is created with default options."""
         # Arrange
@@ -86,6 +88,64 @@ class TestStartLoggingOptions(unittest.TestCase):
         rlf_patch.assert_called_once_with(rate_limit, burst_limit)
 
         logging_patch.captureWarnings.assert_called_with(True)
+
+
+class Test_RateLimitFilter(unittest.TestCase):
+
+    def _record(self, name, levelno):
+        Record = namedtuple("Record", ["name", "levelno"])
+        return Record(name, levelno)
+
+    @unittest.mock.patch("qmi.core.logging_init.logging")
+    def test_filter(self, logging_patch):
+        """Test filter function and counter_data"""
+        # Arrange
+        rate_limit = 5
+        burst_limit = 20
+        name = "record_name"
+        levelno = 5
+        bucket_level = 0
+        discarded_msgs = 1
+        exp_bucket_level = float(bucket_level + rate_limit - 1)
+        exp_disc_msgs = discarded_msgs + 1
+        exp_record = {(name, levelno): (monotonic() + 1, exp_bucket_level, exp_disc_msgs)}
+
+        filter_obj = _RateLimitFilter(rate_limit, burst_limit)
+        filter_obj._counters = {
+            (name, levelno): (monotonic(), bucket_level, discarded_msgs)
+        }
+        # Act
+        res1 = filter_obj.filter(self._record(name, levelno))
+        sleep(1)  # sleep to make (time_now - last_time) larger
+        res2 = filter_obj.filter(self._record(name, levelno))
+
+        # Assert
+        self.assertFalse(res1)
+        self.assertTrue(res2)
+        self.assertDictEqual(exp_record, filter_obj._counters)
+
+    def test_filter_no_counter_data(self):
+        """Test case where gotten counter_data is None."""
+        # Arrange
+        rate_limit = 5
+        burst_limit = 20
+        name = "record_name"
+        levelno = 5
+        exp_bucket_level = float(burst_limit - 1)
+        exp_disc_msgs = 0
+        exp_record = {(name, levelno): (monotonic() + 1, exp_bucket_level, exp_disc_msgs)}
+
+        filter_obj = _RateLimitFilter(rate_limit, burst_limit)
+        filter_obj._counters = {(name, levelno): None}
+        # Act
+        res1 = filter_obj.filter(self._record(name, levelno))
+        sleep(1)  # sleep to make (time_now - last_time) larger
+        res2 = filter_obj.filter(self._record(name, levelno))
+
+        # Assert
+        self.assertTrue(res1)
+        self.assertTrue(res2)
+        self.assertDictEqual(exp_record, filter_obj._counters)
 
 
 if __name__ == '__main__':
