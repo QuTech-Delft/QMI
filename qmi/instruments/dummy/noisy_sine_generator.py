e0735ce576b30b94c62757aa9e473dd014a5cea1
Henri Ervasti <heevasti@tudelft.nl>
2025-08-25 14:02:32 +0200

    [QMI-156] Added docstrings also for the RPC methods.

--- qmi/instruments/dummy/noisy_sine_generator.py
+++ qmi/instruments/dummy/noisy_sine_generator.py
@@ -20,6 +20,11 @@ class NoisySineGenerator(QMI_Instrument):
 
     @rpc_method
     def set_frequency(self, value: float) -> None:
+        """Set the frequency of the sine wave.
+
+        Parameters:
+            value: The frequency value (unitless).
+        """
         self._check_is_open()
         valid = isinstance(value, float) and math.isfinite(value) and value >= 0.0
         if not valid:
@@ -28,11 +33,21 @@ class NoisySineGenerator(QMI_Instrument):
 
     @rpc_method
     def get_frequency(self) -> float:
+        """Get the frequency of the sine wave.
+
+        Returns:
+            self.frequency: The current frequency (unitless).
+        """
         self._check_is_open()
         return self.frequency
 
     @rpc_method
     def set_amplitude(self, value: float) -> None:
+        """Set the amplitude of the sine wave.
+
+        Parameters:
+            value: The new amplitude (unitless).
+        """
         self._check_is_open()
         valid = isinstance(value, float) and math.isfinite(value) and value >= 0.0
         if not valid:
@@ -42,11 +57,21 @@ class NoisySineGenerator(QMI_Instrument):
 
     @rpc_method
     def get_amplitude(self) -> float:
+        """Get the amplitude of the sine wave.
+
+        Returns:
+            self.amplitude: The current amplitude (unitless).
+        """
         self._check_is_open()
         return self.amplitude
 
     @rpc_method
     def set_noise(self, value: float) -> None:
+        """Set noise level for the sine wave.
+
+        Parameters:
+            value: The new noise level (unitless).
+        """
         self._check_is_open()
         valid = isinstance(value, float) and math.isfinite(value) and value >= 0.0
         if not valid:
@@ -55,11 +80,21 @@ class NoisySineGenerator(QMI_Instrument):
 
     @rpc_method
     def get_noise(self) -> float:
+        """Get noise level for the sine wave.
+
+        Returns:
+            self.noise: The current noise level (unitless).
+        """
         self._check_is_open()
         return self.noise
 
     @rpc_method
     def wait(self, duration: float) -> None:
+        """Make driver to wait.
+
+        Parameters:
+            duration: The wait duration in seconds.
+        """
         self._check_is_open()
         if duration < 0.0:
             raise ValueError("Bad value for duration: {!r}".format(duration))
@@ -68,6 +103,12 @@ class NoisySineGenerator(QMI_Instrument):
 
     @rpc_method
     def get_sample(self) -> float:
+        """Get a 'sample' from the virtual instrument sine wave.
+
+        Returns:
+            sample: A time-dependent sample value of sine wave with given frequency and amplitude,
+                    and added Gaussian noise.
+        """
         self._check_is_open()
         t = time.time()
         return self.amplitude * math.sin(t * self.frequency * 2 * math.pi) + random.gauss(0.0, self.noise)
