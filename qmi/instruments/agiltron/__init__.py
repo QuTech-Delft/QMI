1addbcb28a847e9537f9a1878769e3dbbdd23893
heevasti <87366176+heevasti@users.noreply.github.com>
2025-09-12 11:31:24 +0200

    [QMI-165] Added new QMI driver for Agiltron 8-channel optical switch. At the same time the 4-channel OS driver was refactored so that a common base class is used for both drivers. Edited respectively also the unit-tests and CHANGELOG.md (#166)
    Co-authored-by: Henri Ervasti <heevasti@tudelft.nl>

--- qmi/instruments/agiltron/__init__.py
+++ qmi/instruments/agiltron/__init__.py
@@ -3,6 +3,8 @@ Agiltron, optical switch driver device.
 
 The qmi.instruments.agiltron package provides support for:
 - FF 1x4 optical switch driver
+- FF 1x8 optical switch driver
 """
 # Using QMI naming convention approved name
 from qmi.instruments.agiltron.ff1x4 import Agiltron_FF1x4 as Agiltron_Ff1x4
+from qmi.instruments.agiltron.ff1x8 import Agiltron_FF1x8 as Agiltron_Ff1x8
