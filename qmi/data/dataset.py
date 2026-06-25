b75e7a27ba6e18fdec8ce08da015fc9335aeff9b
bvanommen <39768108+bvanommen@users.noreply.github.com>
2026-06-25 17:23:46 +0200

    mypy

--- qmi/data/dataset.py
+++ qmi/data/dataset.py
@@ -788,7 +788,8 @@ def write_dataset_to_text(dataset: DataSet, fh: TextIO) -> None:
             raise ValueError(f"Invalid character ':' in attribute name {name!r}")
 
         attrs[name] = val
-
+        
+    rawdata: np.ndarray # For mypy
     if dataset.data.ndim == 1:
         rawdata = dataset.data.reshape(-1, 1)
     elif dataset.data.ndim > 2:
