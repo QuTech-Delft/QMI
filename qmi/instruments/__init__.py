9b7a579f9505cff38564096d2ee336f8265056a2
Henri Ervasti <heevasti@tudelft.nl>
2026-07-03 14:05:56 +0200

    [stable-0-53] Fixing the unittests that were forgotten in old format while updating to new tag+release.

--- qmi/instruments/__init__.py
+++ qmi/instruments/__init__.py
@@ -1,2 +1,5 @@
 """ QMI drivers for a variety of instruments, driver packages by vendor. Mainly using vendor name for package name.
 """
+import os
+if "QMI_CONFIG" in os.environ:
+    del os.environ["QMI_CONFIG"]
